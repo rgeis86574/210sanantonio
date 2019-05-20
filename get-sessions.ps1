64a5ca52d8bc4c1e8bcb17cc1e20bc8dfa7b7611
rgeis86574 <geissler.robert@gmail.com>
2019-05-20 09:41:29 -0500

    Add files via upload

---
+++ get-sessions.ps1
@@ -0,0 +1,3 @@
+﻿function get-sessions {
+Get-RDUserSession -ConnectionBroker "pbgterm.ml.local" | sort username | Export-Csv c:\users\rob.geissler\currentsessions.csv
+C:\users\rob.geissler\currentsessions.csv}
